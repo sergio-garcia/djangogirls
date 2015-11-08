3705f763d9e20469bf60216aadc54bb0693493ae
Sergio Garcia <sergio@ginx.com.br>
2015-11-08 20:53:53 -0200

    Added views to create/edit blog post inside the site.

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.shortcuts import redirect
 from django.utils import timezone
 from .forms import PostForm
 from .models import Post
